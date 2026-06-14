f4e2dc641b03268d8c4219587880893f00e49e36
Sakhi12 <sakshipanchal021@gmail.com>
2026-06-14 23:50:16 +0530

    initial commit: todoflow-cicd

---
+++ app/test_main.py
@@ -0,0 +1,17 @@
+import pytest
+from app.main import app
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as c:
+        yield c
+
+def test_get_todos_empty(client):
+    res = client.get("/todos")
+    assert res.status_code == 200
+
+def test_add_todo(client):
+    res = client.post("/todos", json={"task": "Buy milk"})
+    assert res.status_code == 201
+    assert res.get_json()["task"] == "Buy milk"
