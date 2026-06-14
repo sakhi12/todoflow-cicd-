45564daba0ddc996f5d6ea97ec8097822e57765c
Sakhi12 <sakshipanchal021@gmail.com>
2026-06-15 00:00:06 +0530

    fix: flake8 lint errors

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 todos = []
 
+
 @app.route("/todos", methods=["GET"])
 def get_todos():
     return jsonify(todos)
+
 
 @app.route("/todos", methods=["POST"])
 def add_todo():
@@ -13,6 +15,7 @@ def add_todo():
     todo = {"id": len(todos) + 1, "task": data["task"], "done": False}
     todos.append(todo)
     return jsonify(todo), 201
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
