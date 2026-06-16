2f89af705622636a9220fadba4d6e09015fbe0cb
Sakhi12 <sakshipanchal021@gmail.com>
2026-06-16 10:39:38 +0530

    fix: clean up main.py and test_main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,19 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+todos = []
+
+
+@app.route("/")
+def home():
+    return jsonify({"status": "TodoFlow CI/CD is running!"})
+
+
+@app.route("/todos", methods=["GET"])
+def get_todos():
+    return jsonify(todos)
+
+
 @app.route("/todos", methods=["POST"])
 def add_todo():
     data = request.get_json()
@@ -6,3 +22,7 @@ def add_todo():
     todo = {"id": len(todos) + 1, "task": data["task"], "done": False}
     todos.append(todo)
     return jsonify(todo), 201
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
